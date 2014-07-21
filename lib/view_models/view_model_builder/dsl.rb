68abd3be820d01d867f1b7ea78fcacd4cf523d8d
DD <timnew.wti@gmail.com>
2014-07-21 22:14:13 +0800

    Enable ViewModelBuilder to work with specific builder

--- lib/view_models/view_model_builder/dsl.rb
+++ lib/view_models/view_model_builder/dsl.rb
@@ -1,8 +1,20 @@
 class ViewModelBuilder
   module DSL
-    def build(klass, &block)
-      ViewModelBuilder
-      .new(klass, self)
+    def build(something, &block)
+      if something.is_a? Class
+        klass = something
+      else
+        klass = something.class
+      end
+
+      if klass.const_defined? :Builder
+        builder_class = klass.const_get :Builder
+      else
+        builder_class = ViewModelBuilder
+      end
+
+      builder_class
+      .new(something, self)
       .execute(&block)
       .result
     end
