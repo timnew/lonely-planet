bccf35fa621089f34247ddbb9acda751b0fd75e0
DD <timnew.wti@gmail.com>
2014-07-24 00:01:01 +0800

    Remove unused DSL

--- lib/view_models/page/section_dsl.rb
+++ lib/view_models/page/section_dsl.rb
@@ -5,25 +5,12 @@ class Page
       mod.extend CachedAttrs # inject dependency module
     end
 
-    def self.section_builder_name(*names)
-      ['section', names].flatten!.join('_').to_sym
-    end
-
     def declare_sections(&block)
       cached_attr :sections do
         SectionListBuilder
         .new(self)
         .execute(&block)
         .result
-      end
-    end
-
-    def section(*names, &block)
-      builder_name = SectionDSL.section_builder_name(*names)
-      define_method builder_name do
-        current = destination[*names]
-        return nil if current.nil?
-        block.call current
       end
     end
   end
@@ -48,23 +35,17 @@ class Page
     end
 
     def section(*names)
-      builder_method = SectionDSL.section_builder_name(*names)
-      if block_given?
-        current = destination[*names]
+      current = destination[*names]
 
-        if current.nil?
-          section_result = nil
-        else
-          section_result = yield current
-        end
-
+      if current.nil?
+        section_result = nil
       else
-        section_result = @host.send builder_method
+        section_result = yield current
       end
 
       result << section_result unless section_result.nil?
-      # rescue ex
-      #   puts "ERROR: #{ex}" # Guard to avoid crash
+    rescue ex
+      puts "ERROR: #{ex}" # Guard to avoid crash
     end
 
     def respond_to?(symbol, include_private = false)
